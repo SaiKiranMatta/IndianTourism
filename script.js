c807b8f4590affeddb6a23810a3adc7964aff18b
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-07-17 19:50:44 +0530

    Adding project to git

---
+++ script.js
@@ -0,0 +1,88 @@
+const logo = document.getElementsByClassName("logo")
+logo[0].addEventListener('click', (e)=> {
+    window.open("index.xhtml", "_self");
+})
+
+
+//Gujarat
+const unityDiv = document.getElementById("unityDiv");
+unityDiv.addEventListener('click', (e) => {
+    window.open("Statue_of_unity.xhtml","_self");
+});
+
+const sunDiv = document.getElementById("sunDiv");
+sunDiv.addEventListener('click', (e) => {
+    window.open("suntemple.xhtml","_self");
+});
+
+const zarwaniDiv = document.getElementById("zarwaniDiv");
+zarwaniDiv.addEventListener('click', (e) => {
+    window.open("zarwani.xhtml","_self");
+});
+
+//Delhi
+const tajdiv = document.getElementById("tajdiv");
+tajdiv.addEventListener('click',(e)=>{
+    window.open("tajmahal.xhtml","_self")
+});
+
+const lotusDiv = document.getElementById("lotusDiv");
+lotusDiv.addEventListener('click', (e) => {
+    window.open("lotustemple.xhtml", "_self")
+});
+
+const iGateDiv = document.getElementById("iGateDiv");
+iGateDiv.addEventListener('click', (e) => {
+    window.open("indiagate.xhtml", "_self")
+});
+
+//Telangana
+const soeDiv = document.getElementById("soeDiv");
+soeDiv.addEventListener('click', (e) => {
+    window.open("Statue_of_equality.xhtml", "_self");
+})
+
+const charDiv = document.getElementById("charDiv")
+charDiv.addEventListener('click', (e) => {
+    window.open("charminar.xhtml","_self");
+});
+
+const ThouDiv = document.getElementById("ThouDiv");
+ThouDiv.addEventListener('click', (e) => {
+    window.open("1000pillar.xhtml","_self")
+});
+
+//Jammu and Kashmir
+const gulmargDiv = document.getElementById("gulmargDiv");
+gulmargDiv.addEventListener('click', (e) => {
+    window.open("gulmarg.xhtml","_self")
+});
+
+const vaishnavdiv = document.getElementById("vaishnavdiv");
+vaishnavdiv.addEventListener('click',(e)=>{
+    window.open("vaishnavdev.xhtml","_self")
+});
+
+//Goa
+const bomJesus = document.getElementById("bomJesus");
+bomJesus.addEventListener('click', (e) => {
+    window.open("bomjesus.xhtml","_self")
+});
+
+const dudhDiv = document.getElementById("dudhDiv");
+dudhDiv.addEventListener('click', (e) => {
+    window.open("dudhsagarfalls.xhtml", "_self")
+});
+
+const anjunaDiv = document.getElementById("anjunaDiv");
+anjunaDiv.addEventListener('click', (e) => {
+    window.open("Anjuna.xhtml", "_self")
+});
+
+
+
+
+
+
+
+
